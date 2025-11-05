9fc820a3d97e4bfee087eb4d43c73048e1265e72
Markus Walker <markus.walker.25@gmail.com>
2025-11-05 10:43:27 -0800

    Default optional parameters to empty string if not used

--- framework/set/resources/sanity/rancher/upgradeRancher.go
+++ framework/set/resources/sanity/rancher/upgradeRancher.go
@@ -37,12 +37,16 @@ func UpgradeRancher(file *os.File, newFile *hclwrite.File, rootBody *hclwrite.Bo
 		terraformConfig.Standalone.RancherHostname + " " + terraformConfig.Standalone.UpgradedRancherTagVersion + " " +
 		terraformConfig.Standalone.UpgradedRancherChartVersion + " " + terraformConfig.Standalone.UpgradedRancherImage
 
-	if terraformConfig.Standalone.FeatureFlags != nil && terraformConfig.Standalone.FeatureFlags.UpgradedTurtles != "" {
-		command += " " + terraformConfig.Standalone.FeatureFlags.UpgradedTurtles
-	}
-
 	if terraformConfig.Standalone.UpgradedRancherAgentImage != "" {
 		command += " " + terraformConfig.Standalone.UpgradedRancherAgentImage
+	} else {
+		command += " \"\""
+	}
+
+	if terraformConfig.Standalone.FeatureFlags != nil && terraformConfig.Standalone.FeatureFlags.UpgradedTurtles != "" {
+		command += " " + terraformConfig.Standalone.FeatureFlags.UpgradedTurtles
+	} else {
+		command += " \"\""
 	}
 
 	command += " || true'"
