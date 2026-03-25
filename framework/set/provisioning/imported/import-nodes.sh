b9bff3c165519350f0761475acec607353d1bca7
Markus Walker <markus.walker.25@gmail.com>
2026-03-25 10:06:37 -0700

    Add imported test cases to sanity and sanity upgrade

--- framework/set/provisioning/imported/import-nodes.sh
+++ framework/set/provisioning/imported/import-nodes.sh
@@ -34,4 +34,40 @@ PEM=/home/${USER}/key.pem
 sudo chmod 600 ${PEM}
 sudo chown ${USER}:${GROUP} ${PEM}
 
-eval "$IMPORT_COMMAND"
+MAX_TOKEN_RETRIES=10
+TOKEN_RETRY_DELAY=10
+TOKEN_ATTEMPT=1
+
+while [[ -z "${IMPORT_COMMAND}" && $TOKEN_ATTEMPT -le $MAX_TOKEN_RETRIES ]]; do
+    echo "Waiting for registration token..."
+    sleep $TOKEN_RETRY_DELAY
+    IMPORT_COMMAND=$(cat /home/$USER/import_command.txt)
+    TOKEN_ATTEMPT=$((TOKEN_ATTEMPT+1))
+done
+
+if [[ -z "${IMPORT_COMMAND}" ]]; then
+    echo "ERROR: Registration token is still empty after waiting."
+    exit 1
+fi
+
+MAX_RETRIES=5
+RETRY_DELAY=15
+ATTEMPT=1
+SUCCESS=0
+
+while [ $ATTEMPT -le $MAX_RETRIES ]; do
+    eval "$IMPORT_COMMAND"
+    EXIT_CODE=$?
+
+    if [ $EXIT_CODE -eq 0 ]; then
+        SUCCESS=1
+        break
+    else
+        sleep $RETRY_DELAY
+        ATTEMPT=$((ATTEMPT+1))
+    fi
+done
+
+if [ $SUCCESS -ne 1 ]; then
+    exit 1
+fi
