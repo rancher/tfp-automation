172496f41b19484e02cd17e902a8a49ac36352b0
Markus Walker <markus.walker.25@gmail.com>
2026-08-21 11:36:08 -0700

    Update v2.13 and v2.14 jobs in the day 2 operations workflow to use environment prime-head

--- framework/set/resources/airgap/rancher/setup.sh
+++ framework/set/resources/airgap/rancher/setup.sh
@@ -129,6 +129,8 @@ install_prime_head_rancher() {
     echo "Installing Rancher"
     helm upgrade --install rancher rancher-${REPO}/rancher --namespace cattle-system --set global.cattle.psp.enabled=false \
                                                                                          --set systemDefaultRegistry=${REGISTRY} \
+                                                                                         --set 'extraEnv[0].name=CATTLE_AGENT_IMAGE' \
+                                                                                         --set "extraEnv[0].value=${RANCHER_AGENT_IMAGE}:v${LATEST_CHART_VERSION}" \
                                                                                          --set hostname=${HOSTNAME} \
                                                                                          ${IMAGE} \
                                                                                          ${VERSION} \
