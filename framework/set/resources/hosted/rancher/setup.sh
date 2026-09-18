b8f55ea6bd31a4a73880dfd713da56ebde53a212
Markus Walker <markus.walker.25@gmail.com>
2026-09-17 17:14:33 -0700

    Fix ingress.tls.source to be set to traefik instead of secret for hosted clusters

--- framework/set/resources/hosted/rancher/setup.sh
+++ framework/set/resources/hosted/rancher/setup.sh
@@ -270,7 +270,7 @@ install_prime_head_rancher() {
                                                                                          ${VERSION} \
                                                                                          --set agentTLSMode=system-store \
                                                                                          --set bootstrapPassword=${BOOTSTRAP_PASSWORD} \
-                                                                                         --set ingress.tls.source=secret \
+                                                                                         --set ingress.tls.source=traefik \
                                                                                          --devel
 }
 
